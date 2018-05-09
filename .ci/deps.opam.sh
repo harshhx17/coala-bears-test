314d9b289b0d9eaa50164005638842a4bb8b8056
Harsh Kumar Bhartiya <ilikegamess@gmail.com>
2018-05-10 01:15:49 +0530

    Use only things that were preinstalled in the package

--- .ci/deps.opam.sh
+++ .ci/deps.opam.sh
@@ -1,16 +1,18 @@
 set -e
 set -x
 
-export TERM=dumb
+TERM=dumb
 
 # Infer commands
 if [ ! -e ~/infer-linux64-v0.7.0/infer/bin ]; then
   wget -nc -O ~/infer.tar.xz https://github.com/facebook/infer/releases/download/v0.7.0/infer-linux64-v0.7.0.tar.xz
   tar xf ~/infer.tar.xz -C ~/
   cd ~/infer-linux64-v0.7.0
-  apt-get install ocaml ocaml-native-compilers camlp4-extra opam -y
   opam init --y
   opam update
+
+  opam pin add --yes --no-action yojson 1.3.3
+  opam pin add --yes --no-action biniou 1.0.6
 
   # See https://github.com/coala/coala-bears/issues/1763
   opam pin add --yes --no-action atdgen 1.10.0
